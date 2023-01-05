e9ba66db7a5cf3dbc4d9324c889c8bd9d2c01de8
Hitesh Chundi <hitesh.chundi@hotmail.com>
2023-01-05 23:35:03 +0530

    Modified the logic of the calculator

--- script.js
+++ script.js
@@ -1,62 +1,19 @@
-//addition function
-function add(a, b) {
-    return a + b;
+let result = document.getElementById('result');
+let buttons = document.getElementsByClassName('btn');
+let screenValue = "";
+
+for (item of buttons) {
+    item.addEventListener('click', (e) => {
+        buttonText = e.target.value;
+        console.log("The button content is ", buttonText);
+        if (buttonText === 'Clear') {
+            screenValue = "";
+            result.value = screenValue;
+        } else if (buttonText === '=') {
+            result.value = eval(screenValue);
+        } else {
+            screenValue += buttonText;
+            result.value = screenValue;
+        }
+    });
 }
-
-//subtraction function
-function subtract(a, b) {
-    return a - b;
-}
-
-//multiplication function
-function multiply(a, b) {
-    return a * b;
-}
-
-//division function
-function divide(a, b) {
-    return a / b;
-}
-
-function operate(op, a, b) {
-    if (op === '+') {
-        return add(a, b);
-    } else if (op === '-') {
-        return subtract(a, b);
-    } else if (op === '*') {
-        return multiply(a, b);
-    } else if (op === '/' && b !== null) {
-        return divide(a, b);
-    } else {
-        return 'Enter a valid input';
-    }
-}
-
-function display(val) {
-    document.getElementById('result').value += val;
-}
-
-function clr() {
-    document.getElementById('result').value = "";
-}
-
-// function myFunction(event) {
-//     if (event.key == '0' || event.key == '1' 
-//         || event.key == '2' || event.key == '3'
-//         || event.key == '4' || event.key == '5' 
-//         || event.key == '6' || event.key == '7'
-//         || event.key == '8' || event.key == '9' 
-//         || event.key == '+' || event.key == '-'
-//         || event.key == '*' || event.key == '/')
-//         document.getElementById("result").value += event.key;
-// }
-
-// var cal = document.getElementById("calcu");
-//         cal.onkeyup = function (event) {
-//             if (event.keyCode === 13) {
-//                 console.log("Enter");
-//                 let x = document.getElementById("result").value
-//                 console.log(x);
-//                 solve();
-//             }
-//         }
